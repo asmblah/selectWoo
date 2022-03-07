100a26a3f4697db469b3abe883e0f64ed7d03ca5
James Jacobs <jamesjacobs1@mac.com>
2022-03-07 14:06:05 +0000

    Fixed issue introduced with JQ3.6.0 where keydown on s2 element did not select matching option
    JQ 3.6.0 introduced a fix for a bug related to setting focus on an element within another elements on focus event. This fix caused an issue in select2 where when a select2 element was focussed and a user typed, rather than selecting a matching option, the dropdown wasn’t filtered.
    
    This change swaps the jQuery trigger focus for a native .focus() call. Tested in JQ 3.4.1 and 3.6.0

--- src/js/select2/core.js
+++ src/js/select2/core.js
@@ -366,7 +366,7 @@ define([
             self.focusOnActiveElement();
         } else {
           // Focus on the search if user starts typing.
-          $searchField.trigger('focus');
+          $searchField[0].focus();
           // Focus back to active selection when finished typing.
           // Small delay so typed character can be read by screen reader.
           setTimeout(function(){
