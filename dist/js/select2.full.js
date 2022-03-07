1ab69e44a61dea20c3beb7efb5fe2b130429a7b3
James Jacobs <jamesjacobs1@mac.com>
2022-03-07 14:06:15 +0000

    Dist files

--- dist/js/select2.full.js
+++ dist/js/select2.full.js
@@ -5641,7 +5641,8 @@ S2.define('select2/core',[
             self.focusOnActiveElement();
         } else {
           // Focus on the search if user starts typing.
-          $searchField.trigger('focus');
+          $searchField[0].focus();
+
           // Focus back to active selection when finished typing.
           // Small delay so typed character can be read by screen reader.
           setTimeout(function(){
