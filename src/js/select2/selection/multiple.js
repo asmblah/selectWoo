eb810e03d28c14fc2fcc962fcce19133498e2d7c
James Jacobs <jamesjacobs1@mac.com>
2022-09-16 16:24:59 +0100

    Remove eariler aria work from multiple

--- src/js/select2/selection/multiple.js
+++ src/js/select2/selection/multiple.js
@@ -12,9 +12,6 @@ define([
   MultipleSelection.prototype.render = function () {
     var $selection = MultipleSelection.__super__.render.call(this);
 
-    // Add combo box role, needed due to aria-expanded use
-    $selection.attr('role', 'combobox');
-
     $selection.addClass('select2-selection--multiple');
     $selection.html(
       '<ul class="select2-selection__rendered" ' +
@@ -33,25 +30,10 @@ define([
 
     MultipleSelection.__super__.bind.apply(this, arguments);
 
-    if (label) {
-      // role="combobox" requires a label
-      this.$selection.attr('aria-label', label);
-    }
-
     this.$selection.on('click', function (evt) {
       self.trigger('toggle', {
         originalEvent: evt
       });
-    });
-
-    // Add and remove aria-controls to/from selection
-    // when shown (in dom) and removed (not in dom)
-    container.on('open', function () {
-      self.$selection.attr('aria-controls', resultsId);
-    });
-
-    container.on('close', function () {
-      self.$selection.removeAttr('aria-controls');
     });
 
     this.$selection.on(
